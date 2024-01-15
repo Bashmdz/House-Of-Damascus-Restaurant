8b2dbc6a0f158c1ecee83261c15a7d6c5572534b
Bashar Zheiman <bashar.zheiman@web.de>
2024-01-15 21:53:00 +0000

    Update booking app models

--- booking_system/models.py
+++ booking_system/models.py
@@ -7,6 +7,9 @@ class Table(models.Model):
     number = models.IntegerField(unique=True)
     capacity = models.IntegerField()
     is_available = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"Table {self.number} | Capacity: {self.capacity} | {'Available' if self.is_available else 'Not Available'}"
@@ -16,6 +19,9 @@ class Guest(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15)
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"Guest: {self.name} | Email: {self.email} | Phone: {self.phone_number}"
@@ -33,6 +39,10 @@ class Booking(models.Model):
     date_time = models.DateTimeField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
     is_cancelled = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"Booking for {self.guest.name} | Table: {self.table.number} | Status: {self.status}"
@@ -42,6 +52,9 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"Menu Item: {self.name} | Price: {self.price}"
